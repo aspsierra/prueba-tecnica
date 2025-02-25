edc51c2fd08fdb16fc318afc3c0c77ae40e5c133
Álvaro Sierra <asp.sierra@gmail.com>
2025-02-25 15:04:16 +0100

    Added filters

--- tasks_backend/tasks_app/views.py
+++ tasks_backend/tasks_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Family, Task
 from .serializers import FamilySerializer, TaskSerializer
+from .filters import TaskFilter
 
 class FamilyViewSet(viewsets.ModelViewSet):
     queryset = Family.objects.all()
@@ -9,6 +11,8 @@ class FamilyViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['state', 'family']
+    filterset_class = TaskFilter
     ordering_fields = ['state', 'due_date']
-    search_fields = ['title', 'description']
+    search_fields = ['title']
