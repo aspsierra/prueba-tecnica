391fc9797a8e64cca2b3cd33ab8c6862b83fccaa
Álvaro Sierra <asp.sierra@gmail.com>
2025-02-25 12:04:07 +0100

    Created base models

--- tasks_backend/tasks_app/models.py
+++ tasks_backend/tasks_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATES = [
+        ('todo', 'Pendiente'),
+        ('doing', 'En progreso'),
+        ('done', 'Completada')
+    ]
+
+    family = models.ForeignKey(Family, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    state = models.CharField()
+    state = models.CharField(max_length=20, choices=STATES, default='todo')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
--- tasks_backend/tasks_backend/settings.py
+++ tasks_backend/tasks_backend/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tasks_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'prueba_tecnica.sqlite3',
     }
 }
 
