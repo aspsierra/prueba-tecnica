81289f52f00d3f1eab979edc0054908d8abb4e49
Álvaro Sierra <asp.sierra@gmail.com>
2025-02-26 12:44:07 +0100

    Added daisyUI, created base router and base navigation

--- tasks_frontend/src/components/HelloWorld.vue
+++ tasks_frontend/src/components/HelloWorld.vue
@@ -1,10 +1,11 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+<script>
+
+export default {
+  props: {
+    msg: String
+  }
+}
+
 </script>
 
 <template>
---
+++ tasks_frontend/src/components/TasksList.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Listado de Tareas</h1>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
